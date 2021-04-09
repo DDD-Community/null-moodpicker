6ce984b5f3e623aafe0e6294b53e3f8043fdf1e4
Gyeongjun Kim <lastpuzly@gmail.com>
2021-04-10 02:35:43 +0900

    feat: signify pick mode state on popup (#18)

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 
 const Popup: React.FC = () => {
+  const [isPickMode, setIsPickMode] = useState<boolean>();
+
+  useEffect(() => {
+    chrome.storage.sync.get("isPickMode", ({ isPickMode }) => {
+      setIsPickMode(isPickMode);
+    });
+  }, []);
+
   return (
-    <h1>Hi</h1>
+    <>
+      <h1>현재 상태 : {isPickMode ? "켜짐" : "꺼짐"}</h1>
+    </>
   );
 }
 

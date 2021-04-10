3118c8d949eca84a059aa826782a2a6e75b76e01
Gyeongjun Kim <lastpuzly@gmail.com>
2021-04-10 22:33:48 +0900

    feat: add pick mode switch on popup (#22)

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
+import styled from "styled-components";
+import { Switch } from "@material-ui/core";
 
 const Popup: React.FC = () => {
   const [isPickMode, setIsPickMode] = useState<boolean>();
@@ -10,10 +12,24 @@ const Popup: React.FC = () => {
     });
   }, []);
 
+  chrome.runtime.onMessage.addListener(({ isPickMode }) => {
+    setIsPickMode(isPickMode);
+  });
+
+  const Container = styled.div`
+    width: 300px;
+    display: flex;
+  `;
+
+  const togglePickMode = () => {
+    chrome.runtime.sendMessage({ isPickMode: !isPickMode })
+  }
+
   return (
-    <>
+    <Container>
       <h1>현재 상태 : {isPickMode ? "켜짐" : "꺼짐"}</h1>
-    </>
+      <Switch checked={isPickMode} onChange={togglePickMode}/>
+    </Container>
   );
 }
 
